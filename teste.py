a3a741031abf1f62356b90febc938b3acb108a01
Taynara <taynaraa91@gmail.com>
2018-08-11 16:52:58 -0300

    My Django Girls app, first commit

---
+++ teste.py
@@ -0,0 +1,23 @@
+
+def oi():
+    print("olá!")
+    print("tudo bem?")
+oi()
+def oi(nome):
+    if nome=="ola":
+        print ("ola ola")
+    elif nome=="sonja":
+        print ('ola sonja')
+    else:
+        print('ola estranho!')
+oi ('taynara')
+def oi (name):
+    print('ola'+name+'!')
+oi ('Abacaxi')
+girls=['rachel','monica','abacaxi','tay']
+for name in girls:
+    oi (name)
+    print ('proxima')
+for i in range(1,6):
+    print (i)
+    
